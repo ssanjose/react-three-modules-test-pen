1e9c98572c4ef39171c7731c47430cca09367fb3
Jewell <jzaballero@academic.rrc.ca>
2021-05-17 09:35:56 -0500

    update lights, add back window, append to div

--- src/pages/classroom/Classroom.js
+++ src/pages/classroom/Classroom.js
@@ -8,24 +8,20 @@ import swivelChair from "../classroom/models/gltf/swivelchair.glb";
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 import { NavBar } from "../home/MainNavBar";
 import * as dat from "dat.gui";
-import { AmbientLight, CameraHelper } from "three";
 
 export const ClassroomA = () => {
   useEffect(() => {
-    const canvas = document.querySelector("canvas.webgl");
     const scene = new THREE.Scene();
     const loader = new GLTFLoader();
     const gui = new dat.GUI();
-
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas
-    });
+    const renderer = new THREE.WebGLRenderer();
 
     const sizes = {
       width: window.innerWidth,
       height: window.innerHeight,
     };
 
+    // Camera
     const camera = new THREE.PerspectiveCamera(
       75,
       sizes.width / sizes.height,
@@ -34,31 +30,32 @@ export const ClassroomA = () => {
     );
 
     scene.add(camera);
+    scene.background = new THREE.Color(0xa3e8ff);
 
-    const controls = new OrbitControls(camera, canvas);
+    const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(1, 2, 0);
     
-    scene.background = new THREE.Color(0xa3e8ff);
     // Lights
-    const pointLightSun = new THREE.PointLight(0xfff0cc, 1, 100);
+    const directionalLightSun = new THREE.DirectionalLight(0xfff0cc, 1, 100);
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0xe3e3e3, 1.25);
 
-    pointLightSun.position.set(20, 20, 7.5);
-    pointLightSun.castShadow = true;
+    directionalLightSun.position.set(20, 20, 7.5);
+    directionalLightSun.castShadow = true;
     
-    scene.add(pointLightSun);
+    scene.add(directionalLightSun);
     scene.add(hemiLight);
-
-    pointLightSun.shadow.mapSize.width = 512; // default
-    pointLightSun.shadow.mapSize.height = 512; // default
-    pointLightSun.shadow.camera.near = 0.5; // default
-    pointLightSun.shadow.camera.far = 500; // default
-
+    
+    directionalLightSun.shadow.mapSize.width = 512; // default
+    directionalLightSun.shadow.mapSize.height = 512; // default
+    directionalLightSun.shadow.camera.near = 0.5; // default
+    directionalLightSun.shadow.camera.far = 500; // default
+    
+    // Load models (GLTF/GLB)
     let classroom;
     let desk;
     let chair;
 
-    // Load models
+    // Classroom
     loader.load(
       classObject,
       function (gltf) {
@@ -73,6 +70,19 @@ export const ClassroomA = () => {
         );
       });
 
+    // Windows // CHANGE TO LESS EXPENSIVE MATERIAL ???
+    const geometry = new THREE.PlaneGeometry(30, 21);
+    const material = new THREE.MeshPhysicalMaterial({transmission: 0.9, opacity: 1, color: 0xffffff, transparent: true, side: THREE.BackSide});
+    const backWindow = new THREE.Mesh(geometry, material);
+
+    backWindow.position.x = -.25;
+    backWindow.position.y = 10.5;
+    backWindow.position.z = 20.25;
+    
+    scene.add(backWindow);
+    backWindow.receiveShadow = true;
+    
+    // Desks
     for (let i = 0; i < 4; i++) {
       for (let j = 0 ; j < 4; j++) {
         loader.load(
@@ -94,6 +104,7 @@ export const ClassroomA = () => {
       }
     }
 
+    // Chairs
     for (let i = 0; i < 4; i++) {
       for (let j = 0 ; j < 8; j++) {
         loader.load(
@@ -115,6 +126,7 @@ export const ClassroomA = () => {
       }
     }
 
+    // Set renderer properties
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     renderer.shadowMap.enabled = true;
@@ -133,15 +145,18 @@ export const ClassroomA = () => {
       renderer.setSize(sizes.width, sizes.height);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
       renderer.toneMapping = THREE.LinearToneMapping;
-
       renderer.physicallyCorrectLights = true;
-
     });
 
     // VR Button
     document.body.appendChild( VRButton.createButton(renderer));
     renderer.xr.enabled = true;
 
+    // Append canvas
+    const container = document.getElementById("class-container");
+    container.appendChild(renderer.domElement);
+
+    // Render Scene
     renderer.setAnimationLoop( function () {
       renderer.render(scene, camera);
     })
@@ -151,9 +166,7 @@ export const ClassroomA = () => {
   return (
     <div className="hero">
       <NavBar />
-      <div className="hero-container">
-        <canvas className="webgl"></canvas>
-      </div>
+      <div id="class-container"></div>
     </div>
   );
 };
