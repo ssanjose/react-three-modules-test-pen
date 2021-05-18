d64d9c44446037faa82ec4539da9714e37a9fe98
Lawrence Valerio <valerio.lawrence@gmail.com>
2021-05-18 08:33:53 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Homepage } from "./pages/home/Homepage.js";
 import { Classroom } from "./test-lv/Classroom-scene/Classroom.js";
 import "tailwindcss/dist/tailwind.min.css";
-import { Classroom2 } from "./test-lv/test-2-vanilla/test-2-vanilla";
 import { Interaction } from "./test-lv/object-interaction/interaction";
-import { Physics } from "./test-lv/physics-practice/physics";
 import { Movement } from "./test-lv/Movement/Movement";
 
 function App() {
@@ -14,9 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/Classroom" component={Classroom} />
-        <Route path="/Classroom2" component={Classroom2} />
         <Route path="/Interaction" component={Interaction} />
-        <Route path="/Physics" component={Physics} />
         <Route path="/Movement" component={Movement} />
       </Switch>
     </BrowserRouter>
